5ad942d34c464c345fd0b060c8847a23f59ccccf
kmeenuin <meenu.wal@gmail.com>
2017-03-09 19:19:00 -0500

    new project created

---
+++ egifter_test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="egifter_test.MvcApplication" Language="C#" %>
---
+++ egifter_test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(egifter_test.Startup))]
+namespace egifter_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
